ae3d7946b6043a75fabe6e8bd1247f5ed99567e6
andikk <andikk@mail.ru>
2020-02-04 15:58:52 +0300

    Добавил разметку

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -5,7 +5,19 @@ const WelcomeScreen = (props) => {
   const {errors = 0} = props;
 
   return (
-    <p>Количество ошибок: {errors}</p>
+    <section className="welcome">
+      <div className="welcome__logo">
+        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
+      </div>
+      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <h2 className="welcome__rules-title">Правила игры</h2>
+      <p className="welcome__text">Правила просты:</p>
+      <ul className="welcome__rules-list">
+        <li>Нужно ответить на все вопросы.</li>
+        <li>Можно допустить {errors} ошибки.</li>
+      </ul>
+      <p className="welcome__text">Удачи!</p>
+    </section>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 ReactDOM.render(
-    <App errors={0}/>,
+    <App errors={3}/>,
     document.getElementById(`root`)
 );
