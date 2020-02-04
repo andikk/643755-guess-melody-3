1e4350397e0e1c6fdecd1344c573ae4e756a56d2
andikk <andikk@mail.ru>
2020-02-04 16:01:48 +0300

    Добавил константу с количеством ошибок

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const Settings = {
+  ERRORS: 3
+};
+
 ReactDOM.render(
-    <App errors={3}/>,
+    <App errors={Settings.ERRORS}/>,
     document.getElementById(`root`)
 );
