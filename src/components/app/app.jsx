1d1b23500c9ce0d3fece0e62744245ead9f06b22
andikk <andikk@mail.ru>
2020-02-11 22:50:00 +0300

    genre-question-screen props

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+import {GameType} from "../../const.js";
 
 
 const welcomeButtonHandler = () => {};
@@ -22,7 +23,7 @@ const App = (props) => {
           <ArtistQuestionScreen question={questions[1]} />
         </Route>
         <Route exact path="/dev-genre">
-          <GenreQuestionScreen />
+          <GenreQuestionScreen question={questions[0]} />
         </Route>
       </Switch>
     </BrowserRouter>
